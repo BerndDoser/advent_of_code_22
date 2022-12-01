6e54708dff182df95f331ce9d613512a4692467e
Bernd Doser <bernd.doser@braintwister.eu>
2022-12-01 16:33:07 +0100

    add day 1

---
+++ day1/code.py
@@ -0,0 +1,11 @@
+calories = [0]
+for line in open('day1/data.txt'):
+    if line == '\n':
+        calories.append(0)
+        continue
+    calories[-1] += int(line.strip())
+
+print("answer part 1: ", max(calories))
+
+calories.sort()
+print("answer part 2: ", sum(calories[-3:]))
