c48c0851ee3d47448405f93175d3a0a44c738417
Bernd Doser <bernd.doser@braintwister.eu>
2022-12-14 21:58:12 +0000

    day 14 first try

---
+++ day14/code.py
@@ -0,0 +1,55 @@
+import sys
+
+rocks = []
+lower = [sys.maxsize, 0]
+upper = [0, 0]
+for line in open("day14/example.txt"):
+    line = line.strip().split('->')
+    one_rock = []
+    for coord in line:
+        coord = list(map(int, coord.strip().split(',')))
+        lower[0] = min(lower[0], coord[0])
+        lower[1] = min(lower[1], coord[1])
+        upper[0] = max(upper[0], coord[0])
+        upper[1] = max(upper[1], coord[1])
+        one_rock.append(coord)
+    rocks.append(one_rock)
+
+dim = [m - n + 1 for m,n in zip(upper, lower)]
+scan = [['.' for x in range(dim[0])] for y in range(dim[1])]
+x_min = lower[0]
+
+for rock in rocks:
+    for i in range(len(rock) - 1):
+        print(rock[i], rock[i+1])
+        for j in range(min(rock[i][0], rock[i+1][0]), max(rock[i][0], rock[i+1][0]) + 1):
+            scan[j - x_min][rock[i][1]] = '#'
+        for j in range(min(rock[i][1], rock[i+1][1]), max(rock[i][1], rock[i+1][1]) + 1):
+            scan[rock[i][0] - x_min][j] = '#'
+
+cycle = 0
+overflow = False
+
+while not overflow:
+    x = 500 - x_min
+    for y in range(dim[1] + 1):
+        if y == dim[1] - 1:
+            overflow = True
+            break
+        print(x,y)
+        if scan[x][y+1] != '.':
+            if scan[x-1][y+1] == '.':
+                x -= 1
+            elif scan[x+1][y+1] == '.':
+                x += 1
+            else:
+                scan[x][y] == 'o'
+                break
+    cycle += 1
+
+for y in range(dim[1]):
+    for x in range(dim[0]):
+        print(scan[x][y], end='')
+    print('')
+
+print('answer:', cycle)
